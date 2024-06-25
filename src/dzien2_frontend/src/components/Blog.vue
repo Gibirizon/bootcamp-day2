ee2d371894ac9106e386b8954b515abaeb119f01
Gibirizon <swojteksz@gmail.com>
2024-06-25 19:52:49 +0200

    Added wpisy funcionality

---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<button @click="pobierzWpisy">Refresh</button>
+		siema blog!
+		{{ wpisy }}
+		<input v-model="nowyBlog" type="text" />
+		<button @click="dodajWpisy">dodaj</button>
+		{{ nowyBlog }}
+	</div>
+</template>
+
+<script>
+import { dzien2_backend } from "declarations/dzien2_backend/index"
+export default {
+	data() {
+		return {
+			wpisy: [],
+		}
+	},
+	methods: {
+		async dodajWpisy() {
+			await dzien2_backend.dodaj_wpis(this.nowyBlog)
+		},
+		async pobierzWpisy() {
+			this.wpisy = await dzien2_backend.odczytaj_wpisy()
+		},
+	},
+}
+</script>
