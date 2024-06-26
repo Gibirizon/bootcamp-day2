a406188946c2e5754dd60facb44f04fc63ebe2be
Gibirizon <swojteksz@gmail.com>
2024-06-26 19:39:48 +0200

    Added deleting buttons and more styling

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,12 +1,20 @@
 <template>
 	<div>
 		<h2 class="text-blue-600">Wpisy na bloga</h2>
-		<button @click="pobierzWpisy">Refresh</button>
-		<div v-for="wpis in wpisy">
-			<p>{{ wpis }}</p>
+		<div class="w-100 flex flex-row-reverse">
+			<button @click="pobierzWpisy" class="bg-blue-600 rounded-md text-white p-4">refresh</button>
 		</div>
-		<input v-model="nowyBlog" type="text" />
-		<button @click="dodajWpisy">dodaj</button>
+		<div class="grid mx-6 gap-4 my-4">
+			<div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+				<p>ID: {{ index }}</p>
+				<p>{{ wpis }}</p>
+				<button @click="deleteWpis(index)" class="bg-blue-600 rounded-md text-white p-4">Usuń wpis</button>
+			</div>
+		</div>
+		<div class="flex justify-center flex-col">
+			<input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4" />
+			<button @click="dodajWpisy" class="bg-blue-600 rounded-md text-white p-4">dodaj</button>
+		</div>
 	</div>
 </template>
 
@@ -22,6 +30,11 @@ export default {
 	methods: {
 		async dodajWpisy() {
 			await dzien2_backend.dodaj_wpis(this.nowyBlog)
+			await this.pobierzWpisy()
+		},
+		async deleteWpis(index) {
+			await dzien2_backend.usun_wpis(index)
+			await this.pobierzWpisy()
 		},
 		async pobierzWpisy() {
 			this.wpisy = await dzien2_backend.odczytaj_wpisy()
